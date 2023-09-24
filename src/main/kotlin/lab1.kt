14c8a99d45bf8399a108532c5e2d7f197e2a7872
IOANNVOLZHSKIY <ivantokarevd@gmail.com>
2023-09-24 13:33:53 +0300

    Added function os constructor's parser with expanding into linear expressions

--- src/main/kotlin/lab1.kt
+++ src/main/kotlin/lab1.kt
@@ -19,6 +19,144 @@ class Stack{
     override fun toString(): String = elements.toString()
 }
 
+fun parser(part: String, variables: List<String>) {
+    val constructorLevel = mutableMapOf<String, Int>() //Словарь размерности конструктора
+    val constructorTerms = mutableMapOf<String, String>() //Словарь для термов внутри конструктора
+    val constructorLinear = mutableMapOf<String, List<String>>() //Словарь для линейных разложений
+
+    var mem = Stack()
+    var temp: String
+
+    for (i in 0..(part.length - 1)) {
+        if (part[i] == '(') {
+
+            constructorLevel[mem.peek().toString()] = 0 // Если скобка открылась, то создаем нулевое значение уровня конструктора
+
+        } else if (part[i].toString() in variables) {
+
+            if (constructorTerms[mem.peek().toString()] != null){ // Если попалась переменная, то сразу вносим в термы конструктора
+                constructorTerms[mem.peek().toString()] += part[i].toString()
+            } else {
+                constructorTerms[mem.peek().toString()] = part[i].toString()
+            }
+
+            constructorLevel[mem.peek().toString()] = constructorLevel[mem.peek().toString()]!! + 1
+
+        } else if ((part[i].toString() == ",") || (part[i].toString() == " ")) {
+
+            continue
+
+        } else if (part[i] == ')') {
+            temp = mem.pop().toString() // Если скобка закрылась, то смотрим последний конструктор в стеке, а дальше варианты
+
+            if ((constructorLevel[temp] == 2) &&
+                (constructorTerms[temp]?.get(0).toString() in variables) &&
+                (constructorTerms[temp]?.get(1).toString() in variables)) { // Арность - 2. Если оба терма - переменные
+
+                constructorLinear[temp] = listOf(
+                    temp + "_0*" + constructorTerms[temp]?.get(0).toString(),
+                    temp + "_1*" + constructorTerms[temp]?.get(1).toString(),
+                    temp + "_2")
+
+            } else if ((constructorLevel[temp] == 2) &&
+                ((constructorTerms[temp]?.get(0).toString() in variables) &&
+                        !(constructorTerms[temp]?.get(1).toString() in variables))) { // Арность - 2. Если первая - переменная, а вторая - нет
+
+                constructorLinear[temp] = listOf(
+                    temp + "_0*" + constructorTerms[temp]?.get(0).toString())
+
+                constructorLinear[constructorTerms[temp]?.get(1).toString()]?.forEach { element ->
+                    var str = temp
+                    str += "_1*"
+                    str += element
+                    constructorLinear[temp] = constructorLinear[temp]!! + str
+                }
+
+                constructorLinear[temp] = constructorLinear[temp]!! + (temp + "_2")
+
+            } else if ((constructorLevel[temp] == 2) &&
+                ((constructorTerms[temp]?.get(1).toString() in variables) &&
+                        !(constructorTerms[temp]?.get(0).toString() in variables))){ // Арность - 2. Если вторая - переменная, а первая - нет
+
+                constructorLinear[temp] = listOf()
+
+                constructorLinear[constructorTerms[temp]?.get(0).toString()]?.forEach { element ->
+                    var str = temp
+                    str += "_0*"
+                    str += element
+                    constructorLinear[temp] = constructorLinear[temp]!! + str
+                }
+
+                constructorLinear[temp] = constructorLinear[temp]!! + (temp + "_1*" + constructorTerms[temp]?.get(1).toString()) + (temp + "_2")
+
+            } else if ((constructorLevel[temp] == 2) &&
+                (!(constructorTerms[temp]?.get(0).toString() in variables) &&
+                        !(constructorTerms[temp]?.get(1).toString() in variables))){
+
+                constructorLinear[temp] = listOf()
+
+                constructorLinear[constructorTerms[temp]?.get(0).toString()]?.forEach { element ->
+                    var str = temp
+                    str += "_0*"
+                    str += element
+                    constructorLinear[temp] = constructorLinear[temp]!! + str
+                }
+
+                constructorLinear[constructorTerms[temp]?.get(1).toString()]?.forEach { element ->
+                    var str = temp
+                    str += "_1*"
+                    str += element
+                    constructorLinear[temp] = constructorLinear[temp]!! + str
+                }
+
+                constructorLinear[temp] = constructorLinear[temp]!! + (temp + "_2")
+
+            } else if ((constructorLevel[temp] == 1) && (constructorTerms[temp]?.get(0).toString() in variables)) { // Арность - 1. Первый терм - переменая
+
+                constructorLinear[temp] = listOf(
+                    temp + "_0*" + constructorTerms[temp]?.get(0).toString(),
+                    temp + "_1")
+
+            } else if ((constructorLevel[temp] == 1) && !(constructorTerms[temp]?.get(0).toString() in variables)) { // Арность - 1. Первый терм - не переменая
+
+                constructorLinear[temp] = listOf()
+
+                // Здесь мы собираем строку, где temp_0 умножается на каждый элемент из вложенного разложенного конструктора в цикле.
+
+                constructorLinear[constructorTerms[temp]?.get(0).toString()]?.forEach { element ->
+                    var str = temp
+                    str += "_0*"
+                    str += element
+                    constructorLinear[temp] = constructorLinear[temp]!! + str
+                }
+
+                constructorLinear[temp] = constructorLinear[temp]!! + (temp + "_1")
+
+            } else if ((constructorLevel[temp] == 0)) { // Арность - 0
+
+                constructorLinear[temp] = listOf(
+                    temp + "_0")
+
+            }
+        } else {
+
+            if (constructorLevel[mem.peek().toString()] != null) {
+                constructorLevel[mem.peek().toString()] = constructorLevel[mem.peek().toString()]!! + 1
+            }
+
+            if ((mem.peek() != null) && (constructorTerms[mem.peek().toString()] != null)) {
+                constructorTerms[mem.peek().toString()] += part[i].toString()
+            } else if ((mem.peek() != null) && (constructorTerms[mem.peek().toString()] == null)) {
+                constructorTerms[mem.peek().toString()] = part[i].toString()
+            }
+
+            mem.push(part[i])
+        }
+    }
+
+    println(constructorLinear[part[0].toString()])
+}
+
 fun main() {
     val input0 = "variables = x, y"
     val input1 = "f(g(x, y)) -> g(h(y), x)"
@@ -26,8 +164,6 @@ fun main() {
 
     var parts = input0.split(" = ")[1].split(", ")
     val variables = parts.toMutableList()
-    val constructorLevel = mutableMapOf<String, Int>() //Словарь размерности конструктора
-    var mem = Stack()
 
     parts = input1.split(" -> ")
     val leftPart_1 = parts[0].trim()
@@ -36,4 +172,7 @@ fun main() {
     parts = input2.split(" -> ")
     val leftPart_2 = parts[0].trim()
     val rightPart_2 = parts[1].trim()
+
+    parser(rightPart_1, variables)
+
 }
